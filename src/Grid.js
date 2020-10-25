b9c3d11d6933918700df8a6b2f41db8051907ad5
Alex Harston <alex@harston.io>
2020-10-25 18:14:36 +0000

    changing colours on click

--- src/Grid.js
+++ src/Grid.js
@@ -3,6 +3,7 @@ import produce from 'immer';
 
 const numRows = 16;
 const numCols = 30;
+const colors = ['blue', 'red', 'green', 'yellow', 'pink', 'gray', 'purple', 'orange'];
 
 const Grid = (props) => {
     const [grid, setGrid] = useState(() => {
@@ -10,13 +11,14 @@ const Grid = (props) => {
         for (let i=0; i < numRows; i++) {
             rows.push(Array.from(Array(numCols), () => 0))
         }
-
+        
         return rows;
     });
     
     console.log(grid);
-
-
+    
+    const random = Math.floor(Math.random() * colors.length);
+    
     return (
         <div style={{
             display: 'grid',
@@ -25,17 +27,17 @@ const Grid = (props) => {
 
             {grid.map((rows, i) => 
              rows.map((col, k) => <div    
-                key={`${i}-${k}`}
-                onClick = {() => {
-                    const newGrid = produce(grid, gridCopy => {
-                        gridCopy[i][k] = grid[i][k] ? 0 : 1;
+             key={`${i}-${k}`}
+             onClick = {() => {
+                 const newGrid = produce(grid, gridCopy => {
+                     gridCopy[i][k] = grid[i][k] ? 0 : 1;
                     })
                     setGrid(newGrid);
                 }}
                 style={{
                     width: 30, 
                     height: 30,
-                    backgroundColor: grid[i][k] ? 'pink' : undefined,
+                    backgroundColor: grid[i][k] ?  colors[random] : undefined,
                     border: 'solid 1px black',
                     }}
                 />
